96665eae98a7da444a29619566417c410c2ebc6f
Timothée Mazzucotelli <pawamoy@pm.me>
2021-04-05 17:24:26 +0200

    Fix scripts

--- update_json.sh
+++ update_json.sh
@@ -2,6 +2,8 @@
 rm -f *.json
 echo "dataTotal = [" >> data_total.json
 echo "dataMonth = [" >> data_month.json
-git rebase --exec "bash each_commit.sh" 56867ce &>/dev/null
+cp each_commit.sh _each_commit.sh
+git rebase --exec "bash _each_commit.sh" 56867ce &>/dev/null
 echo "[null, null]]" >> data_total.json
 echo "[null, null]]" >> data_month.json
+rm _each_commit.sh
