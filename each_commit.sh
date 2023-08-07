a4e33373e65f8f41f692774978680b2c7f6a4867
Timothée Mazzucotelli <pawamoy@pm.me>
2023-08-07 14:11:27 +0200

    Remove useless files

--- each_commit.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-date=$(git show -s --format=%at)000
-data=$(grep -ie download -e stars README.md | grep -Eo '[0-9,]+' | sed 's/,//g' | tr '\n' ' ')
-read total month stars <<<"${data}"
-[ -n "${total}" ] && echo "[${date}, ${total}]," >> data_total.json
-[ -n "${month}" ] && echo "[${date}, ${month}]," >> data_month.json
-exit 0
--- update_json.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-rm -f *.json
-echo "dataTotal = [" >> data_total.json
-echo "dataMonth = [" >> data_month.json
-cp each_commit.sh _each_commit.sh
-git rebase --exec "bash _each_commit.sh" 56867ce &>/dev/null
-echo "[null, null]]" >> data_total.json
-echo "[null, null]]" >> data_month.json
-rm _each_commit.sh
